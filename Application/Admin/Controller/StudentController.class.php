25cab0b35ca107066c5ca4f73a3cc7a936c95af5
jason <jijianqiao@gmail.com>
2015-10-19 21:22:07 +0800

    add profile

---
+++ Application/Admin/Controller/StudentController.class.php
@@ -0,0 +1,19 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+/**
+ * 后台默认控制器
+ * @author jry <598821125@qq.com>
+ */
+class StudentController extends AdminController{
+
+    public function index(){
+
+
+
+        //$this->display();
+    }
+
+
+
+}
